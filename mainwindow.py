83a478b9425116214f7b2a4399a3ff64c6aecf70
mani moh <mani00moh@gmail.com>
2025-05-11 21:09:34 +0330

    example 4

--- mainwindow.py
+++ mainwindow.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import QSize
-from PySide6.QtWidgets import QMainWindow, QPushButton
+from PySide6.QtWidgets import QMainWindow, QPushButton, QLineEdit, QLabel, QVBoxLayout, QWidget
 
 class MainWindow(QMainWindow):
     def __init__ (self):
@@ -7,16 +7,23 @@ class MainWindow(QMainWindow):
         self.setMinimumSize(QSize(400,300))
         self.setWindowTitle("my App")
 
-        self.button = QPushButton("press")
-        self.button.setMaximumSize(QSize(self.width()//4, self.height()//10))
-        #self.button.setCheckable(True)
-        self.button.clicked.connect(self.button_clicked)
+        # self.button = QPushButton("press")
+        # self.button.setMaximumSize(QSize(self.width()//4, self.height()//10))
+        # #self.button.setCheckable(True)
+        # self.button.clicked.connect(self.button_clicked)
+        #
+        # #Set up the button as our central widget
+        # self.setCentralWidget(self.button)
+        self.label = QLabel(self)
+        self.input_text = QLineEdit(self)
+        self.input_text.textChanged.connect(lambda x :self.label.setText(x.upper()))
 
-        #Set up the button as our central widget
-        self.setCentralWidget(self.button)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.input_text)
+        self.layout.addWidget(self.label)
 
-    def button_clicked(self):
+        self.container = QWidget(self)
+        self.container.setLayout(self.layout)
 
-        print(f"button is pressed")
-        self.button.setEnabled(False)
+        self.setCentralWidget(self.container)
 
