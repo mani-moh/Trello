cfccb1514d3639e901302255237428a66b955845
mani moh <mani00moh@gmail.com>
2025-05-11 20:59:34 +0330

    example 3

--- mainwindow.py
+++ mainwindow.py
@@ -7,15 +7,16 @@ class MainWindow(QMainWindow):
         self.setMinimumSize(QSize(400,300))
         self.setWindowTitle("my App")
 
-        self.button = QPushButton("switch on")
+        self.button = QPushButton("press")
         self.button.setMaximumSize(QSize(self.width()//4, self.height()//10))
-        self.button.setCheckable(True)
+        #self.button.setCheckable(True)
         self.button.clicked.connect(self.button_clicked)
 
         #Set up the button as our central widget
         self.setCentralWidget(self.button)
 
-    def button_clicked(self, state):
-        self.button.setText("switch off" if state else "switch on")
-        print(f"button is {"on" if state else "off"}")
+    def button_clicked(self):
+
+        print(f"button is pressed")
+        self.button.setEnabled(False)
 
