509783ff6aea6065e6b16aa95365d76e89208251
mani moh <mani00moh@gmail.com>
2025-05-11 20:15:38 +0330

    commit 1

--- mainwindow.py
+++ mainwindow.py
@@ -1,9 +1,11 @@
+from PySide6.QtCore import QSize
 from PySide6.QtWidgets import QMainWindow, QPushButton
 
 class MainWindow(QMainWindow):
     def __init__ (self):
         super().__init__()
-        self.setWindowTitle("Button Holder App")
+        self.setMinimumSize(QSize(400,300))
+        self.setWindowTitle("my App")
         button = QPushButton("Press Me!")
 
         #Set up the button as our central widget
