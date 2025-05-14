cd752c2a4e117d5e91fa456f930cb661311a4981
mani moh <mani00moh@gmail.com>
2025-05-14 10:24:34 +0330

    example 4

--- mainwindow.py
+++ mainwindow.py
@@ -1,10 +1,14 @@
 from PySide6.QtCore import QSize
+from PySide6.QtGui import QMouseEvent
 from PySide6.QtWidgets import QMainWindow, QPushButton, QLineEdit, QLabel, QVBoxLayout, QWidget
+from utils import clamp
 
 class MainWindow(QMainWindow):
     def __init__ (self):
         super().__init__()
-        self.setMinimumSize(QSize(400,300))
+        self.WINDOW_X = 800
+        self.WINDOW_Y = 600
+        self.setMinimumSize(QSize(self.WINDOW_X,self.WINDOW_Y))
         self.setWindowTitle("my App")
 
         # self.button = QPushButton("press")
@@ -19,12 +23,19 @@ class MainWindow(QMainWindow):
         self.input_text = QLineEdit(self)
         self.input_text.textChanged.connect(lambda x :self.label.setText(x.upper()))
 
+
+        self.label_cords = QLabel(self)
+        self.label_cords.setText(f"{0}x{0}")
+
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.input_text)
         self.layout.addWidget(self.label)
+        self.layout.addWidget(self.label_cords)
 
         self.container = QWidget(self)
         self.container.setLayout(self.layout)
 
         self.setCentralWidget(self.container)
-
+    def mouseMoveEvent (self, e : QMouseEvent):
+        size = self.size()
+        self.label_cords.setText(f"{clamp(e.pos().x(), 0, size.width())}x{clamp(e.pos().y(), 0, size.height())}")
